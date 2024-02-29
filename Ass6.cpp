a53308e4b58037883e353fd80c10665ae6281270
deepusinghrawat <deepusoftdev@gmail.com>
2024-02-29 11:26:52 +0530

    t

---
+++ Ass6.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+     int num1, num2;
+     cout <<"enter a num";
+     cin>>num1 ;
+
+     cout <<"enter a num";
+     cin>>num2 ;
+
+     cout <<"your num is :"<<num1;
+     cout <<"\n your num is :"<<num2;
+
+}
---
+++ Ass7.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+     int a=3, b=4, sum,mul,sub,div,rem;
+
+    sum=a+b;
+    cout << sum;
+
+      mul=a*b;
+    cout << "\n" << mul;
+
+          sub=a-b;
+    cout << "\n" << sub;
+
+          div=a/b;
+    cout << "\n" << div;
+
+          rem=a%b;
+    cout << "\n" << rem;
+}
